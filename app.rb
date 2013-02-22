304e42b6665687969f4eb537951f7ad557e190f2
homakov <homakov@gmail.com>
2013-02-22 19:50:31 +0700

    upd

--- app.rb
+++ app.rb
@@ -7,23 +7,14 @@ get '/' do
 <!doctype html>
 <html>
 <head>
-<title>Sakurity</title>
+<title>OriginMap</title>
 </head>
 <body>
-<h1>Sakurity</h1>
-<div style="">Make your website <i>sakure</i>: <a href="mailto:homakov@gmail.com">homakov@gmail.com</a></div>
+<h1>OriginMap</h1>
 
 <script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-38702231-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+XSS..
 
 </script>
 </body>
